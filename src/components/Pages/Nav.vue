fa690e1a656b6a9584768bacae7379aaf08ae623
cecidragonetti94 <cecidragonetti@hotmail.com>
2021-11-02 17:31:36 -0300

    button

--- src/components/Pages/Nav.vue
+++ src/components/Pages/Nav.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <v-app-bar
-      color="secondary"
-      dense
-      dark
-    >
+    <v-app-bar color="secondary" dense dark>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>My shop</v-toolbar-title>
@@ -19,71 +15,111 @@
         <v-icon>fa-search</v-icon>
       </v-btn>
 
-      <v-menu
-        left
-        bottom
-      >
+      <v-menu left bottom>
         <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
+          <v-btn icon v-bind="attrs" v-on="on">
             <v-icon>fa-ellipsis-v</v-icon>
           </v-btn>
         </template>
 
         <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
             <v-list-item-title>Option {{ n }}</v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
-      
     </v-app-bar>
-    <v-navigation-drawer app v-model="drawer" temporary dark align-center  color="secondary">
-        <v-container class="mt-4 text-center">
-          <v-col>
-            <v-avatar >
-              <img
-                src="https://png.pngtree.com/element_our/png_detail/20181229/vector-shop-icon-png_302739.jpg"
-                alt=""
-              />
-            </v-avatar>
-          </v-col>
-          <v-col>
-            <p class="white--text nt-3 headline">Shopping App</p>
-          </v-col>
-          <v-col>
-              <v-btn color="primary" to="/">Home</v-btn>
-          </v-col>
-          <v-col>
-              <v-btn color="primary" to="/weatherCity">Electronics</v-btn>
-          </v-col>
-          
-           <v-col>
-              <v-btn color="primary" to="/argentina">Jewelery</v-btn>
-          </v-col>
-          <v-col>
-              <v-btn color="primary" to="/zipCode">Men's clothing</v-btn>
-          </v-col>
-           <v-col>
-              <v-btn color="primary" to="/zipCode">Women's clothing"</v-btn>
-          </v-col>
-        </v-container> 
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      temporary
+      dark
+      align-center
+      color="secondary"
+    >
+      <v-container class="mt-4 text-center">
+        <v-col>
+          <v-avatar>
+            <img
+              src="https://png.pngtree.com/element_our/png_detail/20181229/vector-shop-icon-png_302739.jpg"
+              alt=""
+            />
+          </v-avatar>
+        </v-col>
+        <v-col>
+          <p class="white--text nt-3 headline">Shopping App</p>
+        </v-col>
+        <v-col>
+          <v-btn
+            block
+            color="primary"
+            elevation="2"
+            outlined
+            raised
+            rounded
+            to="/"
+            >Home</v-btn
+          >
+        </v-col>
+        <v-col>
+          <v-btn
+            block
+            elevation="2"
+            outlined
+            raised
+            rounded
+            color="primary"
+            to="/weatherCity"
+            >Electronics</v-btn
+          >
+        </v-col>
+
+        <v-col>
+          <v-btn
+            block
+            elevation="2"
+            outlined
+            raised
+            rounded
+            color="primary"
+            to="/weatherCity"
+            >Jewelery</v-btn
+          >
+        </v-col>
+        <v-col>
+          <v-btn
+            block
+            elevation="2"
+            outlined
+            raised
+            rounded
+            color="primary"
+            to="/weatherCity"
+            >Men's clothing</v-btn
+          >
+        </v-col>
+        <v-col>
+          <v-btn
+            block
+            elevation="2"
+            outlined
+            raised
+            rounded
+            color="primary"
+            to="/weatherCity"
+            >Women's clothing"</v-btn
+          >
+        </v-col>
+      </v-container>
     </v-navigation-drawer>
   </div>
 </template>
 <script>
 export default {
-   data() {
+  data() {
     return {
       drawer: true,
     };
   },
-}
+};
 </script>
